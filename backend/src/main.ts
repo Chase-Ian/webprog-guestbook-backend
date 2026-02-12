827a5c6153ca685d9c805de691f8ff8242aa6c8c
Chase Ian Famisaran <clfamisaran@student.apc.edu.ph>
2026-02-12 18:34:07 +0000

    removed api prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,6 @@ async function bootstrapServer(): Promise<any> {
   if (!cachedApp) {
     const app = await NestFactory.create(AppModule);
     app.enableCors(); // Leave this open for now to ensure it works
-    app.setGlobalPrefix('api'); // Add /api prefix to all routes
     await app.init();
     cachedApp = app;
   }
@@ -27,7 +26,6 @@ if (process.env.NODE_ENV !== 'production') {
   async function startLocal() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
-    app.setGlobalPrefix('api'); // Add /api prefix to all routes
     await app.listen(3000);
   }
   startLocal();
